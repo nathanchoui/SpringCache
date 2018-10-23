be12c3a44f98475baa9c4186c87800c4326c6bf3
za-zhangwei002 <wei.zhang@zatech.com>
2018-10-23 15:47:15 +0800

    init project

---
+++ CacheApp/src/main/java/perosonal/nathan/CacheApplication.java
@@ -0,0 +1,15 @@
+package perosonal.nathan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by za-zhangwei002 on 2018/10/23.
+ */
+@SpringBootApplication
+public class CacheApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CacheApplication.class, args);
+    }
+
+}
