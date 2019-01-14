2e62f2edf3c9272fa4ffd12fa7ab4900229e875c
za-zhangwei002 <wei.zhang@zatech.com>
2019-01-14 20:39:31 +0800

    update

--- CaffeineCache/src/main/java/personal/nathan/caffeine/CaffeineApp.java
+++ CaffeineCache/src/main/java/personal/nathan/caffeine/CaffeineApp.java
@@ -1,19 +1,19 @@
-package perosonal.nathan;
+package personal.nathan.caffeine;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 /**
  * Created by za-zhangwei002 on 2018/10/23.
  */
 @SpringBootApplication
 @Slf4j
-public class CacheApplication {
+public class CaffeineApp {
     public static void main(String[] args) {
-        log.info("CacheApplication starts ...");
-        SpringApplication.run(CacheApplication.class, args);
+        log.info("CaffeineApp starts ...");
+        SpringApplication.run(CaffeineApp.class, args);
+        log.info("CaffeineApp started successfully ...");
     }
 
 }
